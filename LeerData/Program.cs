e3b3bd6bfb8f1606fc02056ea2164ac5fb2bd580
Pedro <espinosaduque@gmail.com>
2020-11-18 17:48:26 +0100

    first commit

---
+++ LeerData/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LeerData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Alumnos al curso ASP.NET Core! - Reacts Hooks");
+        }
+    }
+}
