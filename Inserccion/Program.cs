d899d4c8caa67041d170463a1a0ae92087f72e74
Pedro <espinosaduque@gmail.com>
2020-11-20 16:44:53 +0100

    Inserccion en BBDD

---
+++ Inserccion/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+
+namespace LeerData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(var db = new Conexion()){
+                var nuevoInstructor = new Instructor{
+                    Nombre = "Lorenzo",
+                    Apellidos = "Lopez Arias",
+                    Grado = "Master en Computacion"
+                };
+                db.Add(nuevoInstructor);
+
+                var nuevoInstructor2 = new Instructor{
+                    Nombre = "Jose",
+                    Apellidos = "Mariano",
+                    Grado = "Master en Computacion"
+                };
+                db.Add(nuevoInstructor2);
+
+                var estadoTransaccion = db.SaveChanges();
+
+                if(estadoTransaccion == 1){
+                    Console.WriteLine("Transaccion correcta");
+                }else{
+                    Console.WriteLine("Transaccion incorrecta");
+                }
+                
+            }
+        }
+    }
+}
